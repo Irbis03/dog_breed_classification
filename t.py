7b69ab422b4ca8fe655d82f2d6b53556716b940d
Yaromir Ralovets <ralovets.iaa@phystech.edu>
2026-09-16 17:13:21 +0300

    removed t,py(debug script)

--- t.py
+++
@@ -1,8 +0,0 @@
-import mlflow
-
-# Укажите ваш run_id зависшего запуска
-run_id = "8a34a3a204a04bfeafc3a4f67ce746e9"
-
-client = mlflow.tracking.MlflowClient(tracking_uri="sqlite:///mlflow.db")
-client.set_terminated(run_id, status="FINISHED")  # или "FAILED"
-print(f"Запуск {run_id} принудительно закрыт.")
